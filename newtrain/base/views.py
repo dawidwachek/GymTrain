2abec4fa7807a28b98dc5024246fccfeb9940128
dawidwachek <62208885+dawidwachek@users.noreply.github.com>
2023-06-22 22:29:48 +0200

    Add files via upload

--- newtrain/base/views.py
+++ newtrain/base/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def profile(request):
     return render(request, 'profile.html')
